3bf8f7018e459a054733ad19732842f6747bdf81
rosi <rosimartinsoliveira@gmail.com>
2020-05-23 13:48:44 +0100

    create new classes

--- src/components/Burguer/BurguerIngredient/BurguerIngredient.js
+++ src/components/Burguer/BurguerIngredient/BurguerIngredient.js
@@ -10,13 +10,17 @@ const burguerIngredient = (props) => {
                 <div className={classes.Seeds1}></div>
                 <div className={classes.Seeds2}></div>
             </div>
+            <div className={classes.Ketchup}></div>
+            <div className={classes.AbocadoMayo}></div>
             <div className={classes.Cheese}></div>
             <div className={classes.Tofu}></div>
             <div className={classes.Salad}></div>
             <div className={classes.BreadBottom}></div>
-
         </Aux>
     )
 }
 
 export default burguerIngredient;
+
+
+
