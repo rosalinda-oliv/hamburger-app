1144a06b2c4f0818976e7e78ddb1aff831f897bd
rosi <rosimartinsoliveira@gmail.com>
2020-05-27 18:08:44 +0100

    ingredient selector feature added

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -4,18 +4,20 @@ import BurguerIngredient from "./BurguerIngredient/BurguerIngredient";
 import classes from "./Burguer.module.css";
 
 const burguer = (props) => {
-  let transformIngredients =
-   Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurguerIngredient ingredient={igKey + i} typeIngredient={igKey}/>
-    });
-  })
-  .reduce((arr, el) => {
-    return arr.concat(el);
-  }, [])
+  let transformIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return (
+          <BurguerIngredient ingredient={igKey + i} typeIngredient={igKey} />
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
-  if(transformIngredients.lenght === 0) {
-    transformIngredients = <p> Please select ingredients.</p>
+  if (transformIngredients.lenght === 0) {
+    transformIngredients = <p> Please select ingredients.</p>;
   }
 
   return (
