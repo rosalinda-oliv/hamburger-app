614557f63efdaa3c0121a9b76e8ef7fdb0707a7f
rosi <rosimartinsoliveira@gmail.com>
2020-05-27 17:19:18 +0100

    select ingredients

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -3,11 +3,19 @@ import BurguerIngredient from "./BurguerIngredient/BurguerIngredient";
 import classes from "./Burguer.module.css";
 
 const burguer = (props) => {
-  let transformIngredients = Object.keys(props.ingredients).map((igKey) => {
+  let transformIngredients =
+   Object.keys(props.ingredients).map((igKey) => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurguerIngredient ingredient={igKey} />;
+      return <BurguerIngredient ingredient={igKey + i} type={igKey}/>
     });
-  });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el);
+  }, [])
+
+  if(transformIngredients.lenght === 0) {
+    transformIngredients = <p> Please select ingredients.</p>
+  }
 
   return (
     <div className={classes.Burguer}>
