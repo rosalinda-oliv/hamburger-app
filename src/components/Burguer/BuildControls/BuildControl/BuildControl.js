fb2fd3a023e9c251908254d625b8c6bf27829228
rosi <rosimartinsoliveira@gmail.com>
2020-05-27 17:51:25 +0100

    added and more control

--- src/components/Burguer/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burguer/BuildControls/BuildControl/BuildControl.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-
 import classes from './Buildcontrol.module.css';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        {}
-    <button>More</button>
-    <button>Less</button>
+        <div className={classes.Label}>{props.label}</div>
+    <button 
+    disabled={props.disabled}
+    onClick={props.removed}
+    className={classes.Less}
+    > Less </button>
+    <button
+    onClick={props.added}
+    className={props.More}
+    >More</button>
 </div>
 )   
-
 
 export default buildControl;
