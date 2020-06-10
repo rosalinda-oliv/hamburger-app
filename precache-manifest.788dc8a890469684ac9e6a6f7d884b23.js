fb8080cb4c63fb2303be98f7cd9cb236255a88e3
rosalinda-oliv <rosimartinsoliveira@gmail.com>
2020-06-10 21:21:44 +0100

    Updates

---
+++ precache-manifest.788dc8a890469684ac9e6a6f7d884b23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244778b8b3ad0ffd68e9a11a91442f37",
+    "url": "/hamburguer-app/index.html"
+  },
+  {
+    "revision": "959c928d865263f6c4f1",
+    "url": "/hamburguer-app/static/css/main.47c6f692.chunk.css"
+  },
+  {
+    "revision": "a1ea6446edeaf4749d03",
+    "url": "/hamburguer-app/static/js/2.48fc686a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hamburguer-app/static/js/2.48fc686a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959c928d865263f6c4f1",
+    "url": "/hamburguer-app/static/js/main.fd9a9d63.chunk.js"
+  },
+  {
+    "revision": "563418b42d2df0d807c6",
+    "url": "/hamburguer-app/static/js/runtime-main.718e0934.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/hamburguer-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "66824e3fb787c108f98f6a9b1e09b2f5",
+    "url": "/hamburguer-app/static/media/fundo.66824e3f.jpg"
+  }
+]);
