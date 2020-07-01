071df844160a60ad91977ef8b4e23a02a787b87e
rosi <rosimartinsoliveira@gmail.com>
2020-07-01 14:22:12 +0100

    configuring eslint

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -1,30 +1,29 @@
-import React from "react";
-
-import BurguerIngredient from "./BurguerIngredient/BurguerIngredient";
-import classes from "./Burguer.module.css";
+import React from 'react';
+import BurguerIngredient from './BurguerIngredient/BurguerIngredient';
+import classes from './Burguer.module.css';
 
 const burguer = (props) => {
-  let transformIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-      return [...Array(props.ingredients[igKey])].map((_, i) => {
-        return  <BurguerIngredient key={igKey + i} type={igKey} />
-      })
-    })
-    .reduce((arr, el) => {
-      return arr.concat(el);
-    }, []);
+	const { ingredients } = props;
 
-  if (transformIngredients.length === 0) {
-    transformIngredients = <p> Please select ingredients.</p>
-  }
+	let transformIngredients = Object.keys(ingredients)
+		.map((igKey) => {
+			return [...Array(props.ingredients[igKey])].map(
+				(_, i) => <BurguerIngredient key={igKey + i} type={igKey} />,
+			);
+		})
+		.reduce((arr, el) => arr.concat(el), []);
 
-  return (
-    <div className={classes.Burguer}>
-      <BurguerIngredient type="bread-top" />
-      {transformIngredients}
-      <BurguerIngredient type="bread-bottom" />
-    </div>
-  );
+	if (transformIngredients.length === 0) {
+		transformIngredients = <p> Please select ingredients.</p>;
+	}
+
+	return (
+		<div className={classes.Burguer}>
+			<BurguerIngredient type="bread-top" />
+			{transformIngredients}
+			<BurguerIngredient type="bread-bottom" />
+		</div>
+	);
 };
 
 export default burguer;
